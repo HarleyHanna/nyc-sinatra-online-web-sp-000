275092ac7feec7c4401ae1dfed31e4cd86cf2691
Harley Hanna <harleyhannawrit@gmail.com>
2020-12-15 18:04:20 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -32,7 +32,7 @@ class FiguresController < ApplicationController
     redirect to "/figures/#{@figure.id}"
   end
 
-  patch '/figures/:id'
+  patch '/figures/:id' do
     @figure = Figure.find_by_id(params[:id])
     @figure.update(params[:figure])
 
