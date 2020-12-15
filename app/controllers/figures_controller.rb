cb6846428cc5ed5494e1f286d113bc66620c7b53
Harley Hanna <harleyhannawrit@gmail.com>
2020-12-15 17:34:16 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,46 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params['figure'])
+    unless params[:landmark][:name].empty?
+      @figure.landmarks << Figure.create(params[:landmark])
+    end
+
+    unless params[:title][:name].empty?
+      @figure.titles << Figure.create(params[:title])
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  patch '/figures/:id'
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    unless params[:title][:name].empty?
+      @figure.landmarks << Figure.create[:landmark]
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
